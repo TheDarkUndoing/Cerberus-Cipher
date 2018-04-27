45a2354895be0d5283ab4cff2d4eec43c8fccd9c
Connor Jurgensen <cjurgens9@gmail.com>
2018-04-26 20:31:24 -0400

    Add files via upload

--- menu.py
+++ menu.py
@@ -1,17 +1,20 @@
 import argparse
+import base64
 from banner import banner
 
 def menu():
 	#parser = argparse.ArgumentParser(prog='CerberusCipher', description=banner())
 	parser = argparse.ArgumentParser(description=banner())
 	parser.add_argument('-e', '--encrypt', help='encrypt phrase',
-						action='store_true')
+						action='store')
 
 	parser.add_argument('-d', '--decrypt', help='decrypt phrase',
-						action='store_true')
+						action='append')
 
 
 	args = parser.parse_args()
+	base64.standard_b64encode(args)
+	print(args)
 	#print(args.accumulate(args.Help))
 
 menu()

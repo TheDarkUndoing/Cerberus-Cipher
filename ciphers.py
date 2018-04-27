d2306fabfb6137aece74fa5b13c9c3a7d26eb4c4
Connor Jurgensen <cjurgens9@gmail.com>
2018-04-26 20:30:49 -0400

    Add files via upload

---
+++ ciphers.py
@@ -0,0 +1,13 @@
+def cipher(number, word):
+	pre_rot = word;
+	post_rot = pre_rot * num
+	print(post_rot)
+	
+number = input("Enter a number: ")
+word = input("Enter a word: ")
+
+cipher(number, word)
+
+
+
+cipher()
